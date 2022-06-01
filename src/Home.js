856e6e223ed03bfdc2352daf45c3ede04bb07a9b
sohail2000 <msohail1379@gmail.com>
2022-06-01 14:11:07 +0530

    blog-preview

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
-    }
-    const handleClickAgain = (name, e) => {
-            console.log(`hello ` + name, e.target)
-    }
+    const [blogs, setblog] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+    
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => {
-                handleClickAgain('mario', e)
-            }}>Click Again</button>
+            {
+                blogs.map((blog)=>(
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p> written by {blog.author}</p>
+                    </div>
+                ))
+            }
         </div>
      );
 }
