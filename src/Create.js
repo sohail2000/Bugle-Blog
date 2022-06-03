67c1d46abf50e5b4600039105af9545a557465dd
sohail2000 <msohail1379@gmail.com>
2022-06-03 23:48:35 +0530

    route

---
+++ src/Create.js
@@ -0,0 +1,12 @@
+const Create = () => {
+    return ( 
+        <div className="create">
+            <h2>Add a New Blog</h2>
+        </div>
+     );
+}
+ 
+export default Create;
+<div className="create">
+    <h2>Add a New Blog</h2>
+</div>
--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,9 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-
-    const [blogs, setblog] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(()=> {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogss')
-                .then(res=> {
-                    if(!res.ok){
-                        throw Error('could not fetch data for this resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setblog(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        },1000);
-    },[]);
-
+    const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
     return (
         <div className="home">
             {error && <div>{error}</div>}
